034b74f740fbf6415b62945f67f18169087b56d4
my name <carson@felixforyou.ca>
2020-12-22 21:00:51 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "Carson Bain Photography",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "yyuxONiBm04Jj9UulN7oqdyfOrppVWHUZ_MnnKaTPH4",
+        spaceId: "kkahdtur6i1v",
+      },
+    },
+    "gatsby-plugin-postcss",
+  ],
+};
